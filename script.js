127efa078738d2f8fdca8e84819a2f90b5bceaff
denigri <63078539+denigri@users.noreply.github.com>
2021-02-16 22:51:03 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -83,17 +83,55 @@ function onIntersectionObserved(entries) {
 		}
 	});
 }
-
+// Assign json to intersectionObserver
 function activateIndicator(index) {
 	indicators.forEach((indicator, i) => {
 		indicator.classList.toggle('active', i === index);
 	});
 	const titles = document.querySelectorAll('.title');
 	titles.forEach((title, i) => {
-		title.classList.toggle('active1', i === index);
+		title.classList.toggle('activeMain', i === index);
 	});
 	const tags = document.querySelectorAll('.tag');
 	tags.forEach((tag, i) => {
-		tag.classList.toggle('activeone', i === index);
+		tag.classList.toggle('activeTag', i === index);
+	});
+	const slide = document.querySelectorAll('.slide');
+	slide.forEach((slide, i) => {
+		slide.classList.toggle('slide-view', i === index);
+	});
+	const divs = document.querySelectorAll('.slide-inside');
+	divs.forEach((div, i) => {
+		div.classList.toggle('none', i === index);
+	});
+	const mobile = document.querySelectorAll('.mobile');
+	mobile.forEach((mobile, i) => {
+		mobile.classList.toggle('view', i === index);
 	});
 }
+
+// Close button
+function openDropDown() {
+	var element = document.querySelector('.arrow-down-close');
+	element.classList.toggle('open');
+}
+function callback() {
+	const gallery = document.querySelector('.gallery-title');
+	const opacity = document.querySelector('.section');
+	const container = document.querySelector('.container');
+	gallery.classList.toggle('mod');
+	container.classList.toggle('mod');
+	opacity.classList.toggle('mod');
+	const divs = document.querySelector('.none');
+	Changetoggle(divs);
+	divs.classList.toggle('block');
+
+	function Changetoggle() {
+		const mobile = document.querySelector('.view');
+		mobile.classList.toggle('transition');
+		const slide = document.querySelector('.slide-view');
+		slide.classList.toggle('autoY');
+		const gallery = document.querySelector('.gallery-preview');
+		gallery.classList.toggle('hiddenX');
+	}
+}	
